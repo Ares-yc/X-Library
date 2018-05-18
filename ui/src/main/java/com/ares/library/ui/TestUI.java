3536543c469df971469526f57d3f20d292a21509
颜崔 <237387718@qq.com>
2018-05-18 17:09:38 +0800

    删除中文注解

--- ui/src/main/java/com/ares/library/ui/TestUI.java
+++ ui/src/main/java/com/ares/library/ui/TestUI.java
@@ -2,17 +2,6 @@ package com.ares.library.ui;
 
 import android.util.Log;
 
-/**
- * ====================================
- * 作    者：Ares(颜崔)
- * 地    址：https://github.com/Ares-yc
- * 描    述：测试方法
- * 版    本：1.0.0
- * 创建时间：2018/5/18.
- * 修改时间：2018/5/18.
- * ====================================
- */
-
 public class TestUI {
     
     public static void test(){
